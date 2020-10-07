2aacfc42621d1b0d1603d026dea2be1e29066b2b
IvanLuksic <ivanluksic1@gmail.com>
2020-10-07 15:36:54 +0200

    Added blank React project

---
+++ learnio/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <p>LearnIO</p>
+        </div>
+    );
+}
+
+export default App;
