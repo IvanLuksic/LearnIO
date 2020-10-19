0f889b3bc962876b1ee823622ce117e86909b8ef
AndrejVidovic <vidaftw@gmail.com>
2020-10-19 13:02:08 +0200

    added popup

--- learnio/src/App.js
+++ learnio/src/App.js
@@ -1,12 +1,18 @@
-import React from "react";
+import { useScrollTrigger } from "@material-ui/core";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
+import Popup from './components/Popup';
+import Popuplabel from './components/Popuplabel';
 
 function App() {
     return (
         <div className="App">
             <p>LearnIO</p>
+            
+
         </div>
+
     );
 }
 
