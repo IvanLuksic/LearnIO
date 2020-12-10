7693c49d0fc254097f75382569376f679426ed8d
brunogrbavac <bruno.grbavac@outlook.com>
2020-12-10 11:49:53 +0100

    merge sa Andrejen

--- learnio/src/components/student/Matrica.jsx
+++ learnio/src/components/student/Matrica.jsx
@@ -10,11 +10,8 @@ import PopupDialog from '../common/PopupDialog.jsx';
 import {useDispatch, useSelector} from 'react-redux';
 import {topicSelected} from '../../redux/actions/topicID';
 import WrongPU from './WrongPU';
-<<<<<<< HEAD
 import Skeleton from '@material-ui/lab/Skeleton';
-=======
 import NotFound from '../common/NotFound';
->>>>>>> c8eed3a0b01a0f7301082f059ab74f3b4a4655f6
 
 
 const useStyles = makeStyles((theme) => ({
@@ -78,12 +75,15 @@ const fieldToRows=(field,ao,d)=>{
 function Matrica(props)
 {    
     const [noError,setNoError]=useState(()=> true);
-    const changeNoError=()=>{
+    const changeToError=()=>{
       if(noError===true) setNoError(false);
     }
+    const changeToNoError=()=>{
+      if(noError===false) setNoError(true);
+    }
     let dispatch=useDispatch();
-    if(Number(props.match.params.id)){dispatch(topicSelected(props.match.params.id,"Topic"))};
-    if(!Number(props.match.params.id)){changeNoError()};
+    if(Number(props.match.params.id)){changeToNoError();dispatch(topicSelected(props.match.params.id,"Topic"))};
+    if(!Number(props.match.params.id)){changeToError()};
     const [fields, setFields]=useState(()=>{return fakeFetchResponse.Questions});//bilo data.Questions
     const [aoSelected,setAoSelected]=useState(1);
     const [dSelected,setDSelected]=useState(1);
@@ -145,52 +145,57 @@ function Matrica(props)
    const classes = useStyles();
     return(
       <div>
-      {loading? (
-        <div style={{display: "flex", flexDirection: "column",justifyContent:"space-evenly", alignItems:"center"}} className={classes.background}> 
-        {
-          <PopupDialog openPopup={openPopupQuestion} setOpenPopup={setOpenPopupQuestion} clickAway={true} style={{minWidth:'40%',minHeight:'10%'}}>
-            <QuestionPopup ao={matricaAO} d={matricaD} questionToDisplay={questionSelected} setOpenPopup={setOpenPopupQuestion} setOpenPopupWrong={setOpenPopupWrong} field={fields} setFields={setFields}/>
-          </PopupDialog>        
-        }
-        {
-          <PopupDialog openPopup={openPopupWrong} setOpenPopup={setOpenPopupWrong} clickAway={true} style={{minWidth:'40%',minHeight:'10%'}}>
-            <WrongPU closePopup={setOpenPopupWrong} setTopicID={setTopicID}/>
-          </PopupDialog>        
-        }
-        <Grid container direction="column" justify="flex-start" alignItems="center">
-            <Grid container item md={6} direction="row"  justify="center" alignItems="center" >
-                <Grid item xs={11} md={8} className={classes.topicTitle} direction="column" justify="center" alignItems="flex-start"  container>
-                    <Grid item><Typography  xs={11} color="primary" variant="h2" component="h2" className={classes.lobster}>{topicName} {topicID}</Typography></Grid>
-                    <Grid item><p style={{fontSize:'2vh', color: 'black', display: 'block'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></Grid>
+        {noError?
+          <div>
+            {loading? 
+              <div style={{display: "flex", flexDirection: "column",justifyContent:"space-evenly", alignItems:"center"}} className={classes.background}> 
+                {
+                  <PopupDialog openPopup={openPopupQuestion} setOpenPopup={setOpenPopupQuestion} clickAway={true} style={{minWidth:'40%',minHeight:'10%'}}>
+                    <QuestionPopup ao={matricaAO} d={matricaD} questionToDisplay={questionSelected} setOpenPopup={setOpenPopupQuestion} setOpenPopupWrong={setOpenPopupWrong} field={fields} setFields={setFields}/>
+                  </PopupDialog>        
+                }
+                {
+                  <PopupDialog openPopup={openPopupWrong} setOpenPopup={setOpenPopupWrong} clickAway={true} style={{minWidth:'40%',minHeight:'10%'}}>
+                    <WrongPU closePopup={setOpenPopupWrong} setTopicID={setTopicID}/>
+                  </PopupDialog>        
+                }
+                <Grid container direction="column" justify="flex-start" alignItems="center">
+                    <Grid container item md={6} direction="row"  justify="center" alignItems="center" >
+                        <Grid item xs={11} md={8} className={classes.topicTitle} direction="column" justify="center" alignItems="flex-start"  container>
+                            <Grid item><Typography  xs={11} color="primary" variant="h2" component="h2" className={classes.lobster}>{topicName} {topicID}</Typography></Grid>
+                            <Grid item><p style={{fontSize:'2vh', color: 'black', display: 'block'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></Grid>
+                        </Grid>
+                        <Grid item md = {11} xs = {11} sm = {11} spacing={3} container direction="row" justify="center" alignItems="center" >
+                            <DisplayMatrix changeSelected={changeAoDSelected} ar={fieldToRows(fields,matricaAO,matricaD)} aoSelected={aoSelected} dSelected={dSelected}/>
+                        </Grid>
+                    </Grid>
+                </Grid> 
+              </div>
+              :
+              <div className={classes.skeleton}>
+                <Grid container  direction="row"  justify="center" alignItems="center" style={{marginBottom:"5px"}}>
+                  <Grid item style={{margin:"5px"}}><Skeleton variant="reck"  animation="wave" height={200}  width={200}/></Grid> 
+                  <Grid item style={{margin:"5px"}}><Skeleton variant="reck"  animation="wave" height={200}  width={200} /></Grid> 
+                  <Grid item style={{margin:"5px"}}><Skeleton variant="reck"  animation="wave" height={200}  width={200} /></Grid> 
                 </Grid>
-                <Grid item md = {11} xs = {11} sm = {11} spacing={3} container direction="row" justify="center" alignItems="center" >
-                    <DisplayMatrix changeSelected={changeAoDSelected} ar={fieldToRows(fields,matricaAO,matricaD)} aoSelected={aoSelected} dSelected={dSelected}/>
+                <Grid container direction="row"  justify="center" alignItems="center" style={{marginBottom:"5px"}}>
+                  <Grid item style={{margin:"5px"}}><Skeleton variant="reck" animation="wave" height={200}  width={200} /></Grid> 
+                  <Grid item style={{margin:"5px"}} ><Skeleton variant="reck" animation="wave" height={200}  width={200} /></Grid> 
+                  <Grid item style={{margin:"5px"}}><Skeleton variant="reck" animation="wave" height={200}  width={200} /></Grid> 
                 </Grid>
-            </Grid>
-        </Grid> 
-        </div> )
-      :
-      (
-        <div className={classes.skeleton}>
-       
-          <Grid container  direction="row"  justify="center" alignItems="center" style={{marginBottom:"5px"}}>
-            <Grid item style={{margin:"5px"}}><Skeleton variant="reck"  animation="wave" height={200}  width={200}/></Grid> 
-            <Grid item style={{margin:"5px"}}><Skeleton variant="reck"  animation="wave" height={200}  width={200} /></Grid> 
-            <Grid item style={{margin:"5px"}}><Skeleton variant="reck"  animation="wave" height={200}  width={200} /></Grid> 
-          </Grid>
-          <Grid container direction="row"  justify="center" alignItems="center" style={{marginBottom:"5px"}}>
-            <Grid item style={{margin:"5px"}}><Skeleton variant="reck" animation="wave" height={200}  width={200} /></Grid> 
-            <Grid item style={{margin:"5px"}} ><Skeleton variant="reck" animation="wave" height={200}  width={200} /></Grid> 
-            <Grid item style={{margin:"5px"}}><Skeleton variant="reck" animation="wave" height={200}  width={200} /></Grid> 
-          </Grid>
-          <Grid container  direction="row"  justify="center" alignItems="center" style={{marginBottom:"5px"}}>
-            <Grid item style={{margin:"5px"}}><Skeleton variant="reck" animation="wave" height={200}  width={200} /></Grid> 
-            <Grid item style={{margin:"5px"}}><Skeleton variant="reck" animation="wave" height={200}  width={200} /></Grid> 
-            <Grid item style={{margin:"5px"}}><Skeleton variant="reck" animation="wave" height={200}  width={200} /></Grid> 
-          </Grid>
-        </div>
-      )
-    }
+                <Grid container  direction="row"  justify="center" alignItems="center" style={{marginBottom:"5px"}}>
+                  <Grid item style={{margin:"5px"}}><Skeleton variant="reck" animation="wave" height={200}  width={200} /></Grid> 
+                  <Grid item style={{margin:"5px"}}><Skeleton variant="reck" animation="wave" height={200}  width={200} /></Grid> 
+                  <Grid item style={{margin:"5px"}}><Skeleton variant="reck" animation="wave" height={200}  width={200} /></Grid> 
+                </Grid>
+              </div>
+            }
+          </div>
+          :
+          <div>
+            <NotFound/>
+          </div>
+        }
       </div>
     )
 }
