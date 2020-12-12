fac135ba89d4ff6ccd6e7a43842dd9c5ac7dad3f
brunogrbavac <bruno.grbavac@outlook.com>
2020-12-12 14:42:52 +0100

    Offline test veerzija prve iteracije

--- learnio/src/components/admin/AdminTopics.jsx
+++ learnio/src/components/admin/AdminTopics.jsx
@@ -130,7 +130,7 @@ function AdminTopics(props){
     // brisanje topica iz liste
     const handleDelete=(id)=>{
       setData(
-          [ ...data.filter(polje=> ((polje.topic_id!==id)))]
+          [ ...data.filter(polje=> (polje.topic_id!==id))]
       ); 
     };
     // dodavanje novog topica u listu i id tom topicu
@@ -138,9 +138,6 @@ function AdminTopics(props){
    const addQuestion=(value)=>{
       console.log(value);
       var polje=data;
-      let nextID;
-      nextID=polje.length+1;
-      value.topic_id=nextID;
       polje=[...polje,value];
       polje=polje.sort((a,b)=>(a.id-b.id));
       
@@ -154,6 +151,7 @@ function AdminTopics(props){
       setItem(data);
     };
     const requestDeleteTopic=()=>{
+      handleDelete(item);
       console.log("ZAHTJEV ZA Brisanjem: ");
       console.log({id:item});
       const requestOptions = {
@@ -172,8 +170,8 @@ function AdminTopics(props){
       rows=[...rows,{
         id: data[i].topic_id,
         name: data[i].topic_name,
-        course: data[i].course,
-        subject: data[i].subject
+        course: data[i].course_name,
+        subject: data[i].subject_name
       }]
     };
 
@@ -198,7 +196,7 @@ function AdminTopics(props){
                 <DataGrid disableSelectionOnClick={true}  pageSize={5} components={{pagination: CustomPagination,}} rows={rows} columns={columns} />               
             </div>
             <PopupDialog openPopup={open} setOpenPopup={handleClose} clickAway={false} style={{minWidth:'60%',minHeight:'30%'}}>
-              <AddTopicPU closePopup={handleClose} addTopic={addQuestion}/>
+              <AddTopicPU closePopup={handleClose} addTopic={addQuestion}  fetchedTopics={data}/>
             </PopupDialog>
           </div>
           }
