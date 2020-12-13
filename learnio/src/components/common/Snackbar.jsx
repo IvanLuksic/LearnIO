7a2b53974d819a7b21b789de6d3bbfaa07ff83a5
DinoZzo99 <dinozoricic99@gmail.com>
2020-12-13 12:58:56 +0100

    added snackbar file

---
+++ learnio/src/components/common/Snackbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+
+function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props}/>;
+}
+
+// status can be:
+// error - red
+// success - green
+// warning - orange
+// info - blue
+
+export default function CustomSnackbar(props) {
+    return(
+        <Snackbar
+        anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'right',
+        }}
+        open={true}
+        autoHideDuration={5000}
+        onClose={props.handleClose}>
+            <Alert onClose={props.handleClose} severity={props.status}>
+                {props.text}
+            </Alert>
+        </Snackbar>
+    )
+}
