898cae8851ca6aa058784168da991d238a48656b
IvanLuksic <ivanluksic1@gmail.com>
2020-12-14 00:18:37 +0100

    Fixed

--- config/config.js
+++ config/config.js
@@ -18,5 +18,12 @@ module.exports= {
     use_env_variable: "DATABASE_URL",
     url: process.env.DATABASE_URL,
     dialect: "postgres",
+    "dialectOptions": {
+      "ssl": {
+        "require": true,
+        rejectUnauthorized: false
+
+      }
+    }
   },
 }
