d267f4d58a989bcaa1af9b0d1a306d3dbd095a78
IvanLuksic <ivanluksic1@gmail.com>
2020-12-14 09:04:22 +0100

    Procfile change

--- app.js
+++ app.js
@@ -13,4 +13,9 @@ async function start()
         nodelogger.error(error);
     }
 }
+
+    app.use(express.static(path.join(__dirname, '/client')))
+    app.get('*', (req,res) => {
+      res.sendFile(path.join(__dirname, '/client', 'index.html'))
+    })
 start();
