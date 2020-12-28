ee36d1966940b850fca8c6e8d5b483a76fb7bf49
brunogrbavac <bruno.grbavac@outlook.com>
2020-12-28 11:44:50 +0100

    fiksiran speed dial i privremeno dovršen subject view za studenta preko kojeg se otvaraju topici za odabrani subject klikom na karticu subjecta

---
+++ learnio/src/pages/StudentSubject.jsx
@@ -0,0 +1,29 @@
+import React,{useState} from 'react';
+import {useDispatch} from 'react-redux';
+import {topicSelected} from '../redux/actions/topicID';
+import NotFound from '../components/common/NotFound';
+import StudentTopics from '../components/student/StudentTopics';
+
+
+function StudentSubject(props){
+    const [noError,setNoError]=useState(()=> true);
+    const changeToError=()=>{if(noError===true) setNoError(false);}
+    const changeToNoError=()=>{if(noError===false) setNoError(true);}
+    // let dispatch=useDispatch();
+    if(Number(props.match.params.class_id && Number(props.match.params.subject_id))){changeToNoError();};//dispatch(subjectSelected(props.match.params.id,"Topic"))
+    if((!Number(props.match.params.class_id))||(!Number(props.match.params.subject_id))){changeToError()};
+
+
+    return(
+        <div>
+            {noError?
+                <StudentTopics subject_id={props.match.params.subject_id} class_id={props.match.params.class_id}/>
+                :
+                <NotFound/>
+            }
+        </div>
+);
+}
+export default StudentSubject;
+
+
