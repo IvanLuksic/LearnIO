df31953ca8e708e049d2ca4c975fd59f29b4809c
IvanLuksic <ivanluksic1@gmail.com>
2021-02-09 11:50:23 +0100

    Merge fix

--- client/src/redux/actions/topicID.js
+++ client/src/redux/actions/topicID.js
@@ -1,15 +1,7 @@
-<<<<<<< HEAD:client/src/redux/actions/topicID.js
- export  const topicSelected=(n,m)=>{
-    return{
-        type:'TOPIC_SELECTED',
-        id: n,
-        name: m
-=======
  export  const topicSelected=(n)=>{
     return{
         type:'TOPIC_SELECTED',
         id: n,
->>>>>>> frontend:learnio/src/redux/actions/topicID.js
     };
 };
 
